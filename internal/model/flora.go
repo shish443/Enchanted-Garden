722542da03f142c82b2d38fd1a8fce3183052b74
shish443 <shishkinn0704@gmail.com>
2026-06-01 13:00:53 +0300

    work model/flora.go

--- internal/model/flora.go
+++ internal/model/flora.go
@@ -1,2 +1,18 @@
 // Enchanted Garden/model/flora.go
 package model
+
+import "time"
+
+type Flora struct {
+	ID        uint       `gorm:"primaryKey" json:"id"`
+	BranchID  uint       `gorm:"not null;index" json:"branch_id"`
+	FullName  string     `gorm:"type:varchar(200);not null" json:"full_name"`
+	Position  string     `gorm:"type:varchar(200);not null" json:"position"`
+	HiredAt   *time.Time `gorm:"type:date" json:"hired_at"`
+	CreatedAt time.Time  `json:"created_at"`
+}
+type PlantFloraReq struct {
+	FullName string     `json:"full_name"`
+	HiredAt  *time.Time `json:"hired_at"`
+	Position string     `json:"position"`
+}
